59e0d055f6daf240d20b3670058c051abe15b01c
diwakar1593 <diwakar200kumar@gmail.com>
2022-12-30 18:38:05 +0530

    updated

---
+++ Day1/answer4.js
@@ -0,0 +1,8 @@
+
+var str = "This is string type."; // string data type
+
+var bool = true;  // boolean data type
+
+var x; //undefined data type
+
+var value = null; // null data type
---
+++ Day1/answer7.js
@@ -0,0 +1,4 @@
+var num = 10;
+var str = "ABC";
+var bool = false;
+var num2 = 20;
