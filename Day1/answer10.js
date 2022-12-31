17e81c60ee7d506b913033cd37e2159a99598e71
diwakar1593 <diwakar200kumar@gmail.com>
2022-12-31 15:19:56 +0530

    update

--- Day1/answer10.js
+++ Day1/answer10.js
@@ -1,4 +1,4 @@
 let myAge = 25
 let yourAge = 30
-console.log("I am "+myAge+" years old.");
-console.log("You are "+yourAge+" years old.");
+console.log("I am",myAge,"years old.");
+console.log("You are",yourAge,"years old.");
