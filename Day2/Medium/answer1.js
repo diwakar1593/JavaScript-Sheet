395026579b59bd8751dedcfbbd6c395c25d91349
diwakar1593 <diwakar200kumar@gmail.com>
2023-01-04 15:55:07 +0530

    day 2

--- Day2/Medium/answer1.js
+++ Day2/Medium/answer1.js
@@ -1,2 +1,3 @@
-let quote = "The quote 'There is no exercise better for the heart than reaching down and lifting people up.' by John Holmes teaches us to help one another."
+const quote = "The quote 'There is no exercise better for the heart than reaching down and \
+lifting people up.' by John Holmes teaches us to help one another."
 console.log(quote)
