9117949c2255150f68d0f2f195ecce5568d901ac
diwakar1593 <diwakar200kumar@gmail.com>
2023-01-06 23:43:57 +0530

    hard

---
+++ Day2/Hard/answer1.js
@@ -0,0 +1,3 @@
+const string ='Love is the best thing in this world. Some found their love and some are still looking for their love.' 
+var ptrn = /love/g
+console.log(string.match(ptrn).length)
---
+++ Day2/Hard/answer4.js
@@ -0,0 +1,6 @@
+var baseSalary = 5000;
+var annualBonus = 10000;
+var onlineCourses = 15000;
+
+var totalSalary = (baseSalary*12) + annualBonus + (onlineCourses * 12);
+console.log(totalSalary);
