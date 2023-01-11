69bc1b385260a618933ef3ad98917e706891045b
diwakar1593 <diwakar200kumar@gmail.com>
2023-01-11 18:44:21 +0530

    Medium

---
+++ Day3/Medium/answer3.js
@@ -0,0 +1,10 @@
+/* 
+Get length and width using prompt and calculate an area of rectangle (area = length x width and the perimeter of rectangle (perimeter = 2 x (length + width))
+*/
+
+let length = 20           //prompt("Enter length : ","numbers goes here")
+let width = 10         //prompt("Enter width : ","numbers goes here")
+const area = length * width
+const perimeter = 2 * (length + width)
+console.log('The area of the rectangle is '+ area)
+console.log('The perimeter of the rectangle is '+ perimeter)
---
+++ Day3/Medium/answer7.js
@@ -0,0 +1,16 @@
+/*
+Compare the slope of above two questions.
+*/
+// Slope from 1st question 
+
+let m1 = 2
+
+// Slope from 2nd question 
+
+let x1 = 2 
+let y1 = 2 
+let x2 = 6 
+let y2 = 10
+const m2 = (y2 - y1) / (x2 - x1)
+
+console.log(m1 == m2)
