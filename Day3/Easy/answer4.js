5c2781663cf5f5eff83ebeacb3d579572b9d17f7
diwakar1593 <diwakar200kumar@gmail.com>
2023-01-11 15:43:29 +0530

    Easy

---
+++ Day3/Easy/answer4.js
@@ -0,0 +1,11 @@
+//1.  Write three JavaScript statement which provide truthy value.
+
+console.log(3 >= 2)
+console.log(0 == '')
+console.log(0 == false)
+
+//2. Write three JavaScript statement which provide falsy value.
+
+console.log(3 === '3')
+console.log(3 < 2)
+console.log(3 != 3)
