c5ebb4738db6b1c1cde6271e16ae15d50324fc31
diwakar1593 <diwakar200kumar@gmail.com>
2023-01-11 23:17:00 +0530

    Medium

---
+++ Day3/Medium/answer10.js
@@ -0,0 +1,9 @@
+let name = 'Diwakar'        //prompt("Enter your name : ","name goes here")
+
+if(name.length > 7 ){
+    console.log('Your name is long')
+}
+else{
+    console.log('Your name is short')
+}
+
---
+++ Day3/Medium/answer11.js
@@ -0,0 +1,13 @@
+/*Compare your first name length and your family name length and you should get this output.
+let firstName = 'Asabeneh'
+let lastName = 'Yetayeh'
+Your first name, Asabeneh is longer than your family name, Yetayeh */
+
+let firstName = 'Diwakar'        
+let lastName = 'Gupta'        
+if(firstName.length > lastName.length){
+    console.log('Your first name, ' + firstName + ' is longer than family name '+ lastName)
+}
+else{
+    console.log('Your first name, ' + firstName + ' is shorter than family name '+ lastName)
+}
