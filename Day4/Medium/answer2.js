7005c7a67ef4e5f864e6bba8b21457b1ddd75fc1
diwakar1593 <diwakar200kumar@gmail.com>
2023-01-15 18:49:24 +0530

    updated

--- Day4/Medium/answer2.js
+++ Day4/Medium/answer2.js
@@ -10,41 +10,25 @@ let monthUserInput = 'October'   //prompt("Enter a month : ","month goes here")
 let month = monthUserInput.toLowerCase()
 
 switch (month) {
+    case 'december':
     case 'january':
-        console.log('The season is Winter.')
-        break
     case 'february':
         console.log('The season is Winter.')
         break
     case 'march':
-        console.log('The season is Spring.')
-        break
     case 'april':
-        console.log('The season is Spring.')
-        break
     case 'may':
         console.log('The season is Spring.')
         break
     case 'june':
-        console.log('The season is Summer.')
-        break
     case 'july':
-        console.log('The season is Summer.')
-        break
     case 'august':
         console.log('The season is Summer.')
         break
     case 'september':
-        console.log('The season is Autumn.')
-        break
     case 'october':
-        console.log('The season is Autumn.')
-        break
     case 'november':
         console.log('The season is Autumn.')
-        break
-    case 'december':
-        console.log('The season is Winter.')
         break
     default:
         console.log('It is not a month of a year.')
