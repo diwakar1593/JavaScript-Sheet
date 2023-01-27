10966149e5cb5958a9942c54735f0b979ad0fef1
diwakar1593 <diwakar200kumar@gmail.com>
2023-01-27 23:53:03 +0530

    Day6

---
+++ Day6/Easy/answer1.js
@@ -0,0 +1,18 @@
+// Iterate 0 to 10 using for loop, do the same using while and do while loop.
+
+// using for loop
+for(let i=0; i<=10; i++){
+
+}
+
+// using while loop
+let i=0
+while(i<=10){
+    i++
+}
+
+// using do while loop
+let j=0
+do{
+    j++
+}while(j<=10)
---
+++ Day6/Easy/answer8.js
@@ -0,0 +1,6 @@
+//Use for loop to iterate from 0 to 100 and print only odd numbers
+
+for(let i=0; i<=100; i++){
+    if(i%2!=0)
+    console.log(i);
+}
